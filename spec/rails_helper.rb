8bc6e37033d1db9520dc984ba1b969af48c8c4d9
Thiago Durante Pires <thdurante@gmail.com>
2018-08-07 18:02:07 -0300

    Fix database cleaner and spec/rails_helper config files.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,8 +35,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
-  # Devise
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
