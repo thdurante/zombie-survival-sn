5eebedcb031324251a1d4668daeabe174d017214
Thiago Durante Pires <thdurante@gmail.com>
2018-08-08 20:51:37 -0300

    Create uniqueness validation in InfectionAlert model. Survivor cannot report other survivor more than once.

--- app/models/infection_alert.rb
+++ app/models/infection_alert.rb
@@ -3,4 +3,5 @@ class InfectionAlert < ApplicationRecord
   belongs_to :author, class_name: 'Survivor'
 
   validates :infected_survivor, :author, presence: true
+  validates :infected_survivor_id, uniqueness: { scope: :author_id }
 end
--- spec/models/infection_alert_spec.rb
+++ spec/models/infection_alert_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe InfectionAlert, type: :model do
+  subject { create(:infection_alert) }
+
   describe 'associations' do
     it { is_expected.to belong_to(:author).class_name('Survivor') }
     it { is_expected.to belong_to(:infected_survivor).class_name('Survivor') }
@@ -9,5 +11,6 @@ RSpec.describe InfectionAlert, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:author) }
     it { is_expected.to validate_presence_of(:infected_survivor) }
+    it { is_expected.to validate_uniqueness_of(:infected_survivor_id).scoped_to(:author_id) }
   end
 end
