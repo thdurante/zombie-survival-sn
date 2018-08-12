ceb2c21a0315a7444c32c7637199927b569e5a48
Thiago Durante Pires <thdurante@gmail.com>
2018-08-12 17:50:12 -0300

    Create trade_item scopes for Item model.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe Item, type: :model do
     it { is_expected.to validate_presence_of(:kind) }
     it { is_expected.to validate_inclusion_of(:kind).in_array(TradeItem.names.map(&:to_s)) }
   end
+
+  describe 'scopes' do
+    TradeItem.names.each do |trade_item|
+      describe ".#{trade_item}" do
+        (TradeItem.names - Array(trade_item)).each do |different_trade_item|
+          let!(:"#{different_trade_item}_list") { create_list(:item, 3, kind: different_trade_item) }
+        end
+
+        let!(:"#{trade_item}_list") { create_list(:item, 3, kind: trade_item) }
+
+        subject { described_class.send(trade_item) }
+
+        it "returns the items with #{trade_item} kind" do
+          is_expected.to match_array(send("#{trade_item}_list"))
+        end
+      end
+    end
+  end
 end
